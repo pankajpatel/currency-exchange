76845e12eaabc870e03fe4332d4190130334cf5c
pankajpatel <pankajpatel@users.noreply.github.com>
2020-05-04 01:58:36 +0000

    deploy: f8b01386f31876ac62a440f9a6789fdbbb05633c

---
+++ precache-manifest.5042e8bfaafeeec47e23ad82329a8451.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bf8203f3b57961e0142a32d406d487e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b684da5786a7dcf05042",
+    "url": "/static/css/2.47a6574b.chunk.css"
+  },
+  {
+    "revision": "ffc12546dfe4c1b5ca0e",
+    "url": "/static/css/main.3868dcab.chunk.css"
+  },
+  {
+    "revision": "b684da5786a7dcf05042",
+    "url": "/static/js/2.1558927d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1558927d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffc12546dfe4c1b5ca0e",
+    "url": "/static/js/main.7ca74425.chunk.js"
+  },
+  {
+    "revision": "04715ca438f81d2075fc",
+    "url": "/static/js/runtime-main.f2ac96f6.js"
+  }
+]);
